4d083543d0f68f0cd3887e449493d49a55502154
Sala213 <Sala213@ezse.pl>
2025-09-26 13:14:54 +0200

    Drugi napisany program (VariableConsoleApplication 3)

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -1,5 +1,18 @@
 ﻿#include <iostream>
 
+/*
+*.
+*. Program obliczający objętość stożka.
+*. Program obliczający pole koła.
+*. Program obliczający wartość wyrażenia a^2 + b^2
+*. Program obliczający pole trójkąta o podstawie b i wysokości h
+*. Program obliczający objętość kuli o promieniu r
+*. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+*. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+*. Stwórz program, który pobiera od użytkownika długość w metrach i przelicza ją na centymetry i milimetry, wyświetlając wynik.
+*. Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
+*/
+
 //Napisz program który wczyta liczbę od użytkownika i ją wyświetl na konsoli
 void task1()
 { 
@@ -24,13 +37,30 @@ void task1()
 //Program obliczający średnią arytmetyczną dwóch liczb.
 void task2()
 {
+    int firstNumber, secondNumber;
+    std::cout << "Podaj pierwszą liczbę:\n";    
+    std::cin >> firstNumber;
+
+
+    std::cout << "Podaj drugą liczbę:\n";
+    std::cin >> secondNumber;
+
+    int avg;
+
+    avg = (firstNumber + secondNumber) / 2;
+
+    std::cout << "Wynik średniej: " << avg << "/n";
+}
+//Program obliczający pole prostokąta.
+void task3()
+{
 
 }
 
 
 int main()
 {
-    task1();
+    task3();
 }
 
 /*
@@ -44,6 +74,15 @@ typ_zmienej nazwa_zmienej;
 Typ zmiennej - wielkość obszaru pamięci, interpretacja ciągu bitów
 
 int - 4 bajtowy liczba całkowita ze znakiem <-2 147 483 648, 2 147 483 647>
+short - 2 bajtowa liczba całkowita ze znakiem <-32 768, 32 767>
+long - 4 bajtowa liczba całkowita ze znakiem <-2 147 483 648, 2 147 483 647>
+long long - 8 bajtowa liczba ze znakiem <-9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
+
+unsigned - zmienna bez znaku <0, 2*max + 1>
+
+float - 4 bajtowa liczba rzeczywista, dokładność 6-7 cyfr po przecinku
+double - 8 bajtowa liczba rzeczywista, dokładność 15-16 cyfr po przecinku
+long double - 12 bajtowa liczba rzeczywista, dokładność 19-20 cyfr po przecinku
 
 Nazwa zmiennej - nazwa obszaru w pamięci, identyfikator
 Warunki niezbędne:
