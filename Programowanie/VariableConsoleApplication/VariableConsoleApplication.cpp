67f239d0a2985abc691e8bc842028993e6f0ee53
Sala213 <Sala213@ezse.pl>
2025-09-30 09:54:19 +0200

    Kilka następnych napisanych programów (VariableConsoleApplication 4)

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -2,10 +2,10 @@
 
 /*
 *.
-*. Program obliczający objętość stożka.
-*. Program obliczający pole koła.
-*. Program obliczający wartość wyrażenia a^2 + b^2
-*. Program obliczający pole trójkąta o podstawie b i wysokości h
+*. 
+*. 
+*. 
+*. 
 *. Program obliczający objętość kuli o promieniu r
 *. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
 *. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
@@ -54,13 +54,83 @@ void task2()
 //Program obliczający pole prostokąta.
 void task3()
 {
+    int sideA, sideB;
+    std::cout << "Podaj wartość pierwszego boku:\n";
+    std::cin >> sideA;
+
+    std::cout << "Podaj wartość drugiego boku:\n";
+    std::cin >> sideB;
+
+    int area;
+
+    area = (sideA * sideB);
+
+    std::cout << "Wynik pola prostokąta: " << area << "\n";
+}
+//Program obliczający objętość stożka.
+void task4()
+{
+    int coneHeight, ray, π;
+    std::cout << "Podaj promień stożka:\n";
+    std::cin >> ray;
+
+    std::cout << "Podaj wysokość stożka:\n";
+    std::cin >> coneHeight;
+    
+    π = 3, 14;
+
+    int bulk;
+
+    bulk = (π * ray * ray * coneHeight) / 3;
+    
+    std::cout << "Wynik objetość stożka: " << bulk << "\n";
+}
+//Program obliczający pole koła.
+void task5()
+{ 
+    int π, rayCircle;
+    std::cout << "Podaj promień koła:\n";
+    std::cin >> rayCircle;
+    
+    π = 3, 14;
+
+    int areaCircle;
+
+    areaCircle = π * (rayCircle * rayCircle);
+
+    std::cout << "Wynik pola koła: " << areaCircle << "\n";
+}
+//Program obliczający wartość wyrażenia a^2 + b^2
+void task6()
+{
+    int valueA, valueB;
+    std::cout << "Podaj wartość a:\n";
+    std::cin >> valueA;
+     
+    std::cout << "Podaj wartość b:\n";
+    std::cin >> valueB;
+
+    int expressionValue;
+
+    expressionValue = (valueA * valueA) + (valueB * valueB);
+
+    std::cout << "Wynik wartość wyrazenia: " << expressionValue << "\n";
+}
+//Program obliczający pole trójkąta o podstawie b i wysokości h
+void task7()
+{
+    int sideB, heightTriangle;
+    std::cout << "Podaj wartość boku b: \n";
+    std::cin >> heightTriangle;
+
+    std::cout << "Podaj wysokość tr"
+
+
 
 }
-
-
 int main()
 {
-    task3();
+    task7();
 }
 
 /*
