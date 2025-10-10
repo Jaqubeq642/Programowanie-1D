e6f8261ccb95f67959bc088dcc1e4de0f7fa00ae
Sala213 <Sala213@ezse.pl>
2025-10-10 13:18:25 +0200

    Pierwszy program z IF (InstructionIFConsoleApplitacion 1)

---
+++ Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+//Napisz program, ktory wyświetli informacje czy liczba jest dodatnia czy nie
+void task1()
+{
+	int number;
+	std::cout << "Podaj liczbą całkowitą: \n";
+	std::cin >> number;
+
+	if (number > 0)
+		std::cout << "Podana liczba jest dodatnia\n";
+
+	std::cout << "Koniec programu";
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+
+	task1();
+}
