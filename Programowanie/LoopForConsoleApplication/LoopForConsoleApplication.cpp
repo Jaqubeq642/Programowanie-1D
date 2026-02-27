79a5ac22f9de86d431f2e8046546b22d4963b421
Sala213 <Sala213@ezse.pl>
2026-02-27 13:10:58 +0100

    pierwsze zadania z petlą for (LoopForConsoleApplication 1 )

--- Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -3,16 +3,40 @@
 
 void task1()
 {
+	int howManyTimes;
+	std::cout << "Podaj ile razy wyświetlać\n";
+	std::cin >> howManyTimes;
 
+	for (int i = 0; i < howManyTimes; i++)
+	{
+		std::cout << "Hello world\n";
+	}
 
+}
+void task2()
+{
+	int upperRange;
+	std::cout << "Podaj górną granice do wyświetlenia\n";
+	std::cin >> upperRange;
 
-
-
-
+	for (int i = 0; i <= upperRange; i += 2)
+	{
+		std::cout << i << "\n";
+	}
+}
+void task3()
+{
+	int sum = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		//std::cout << i << "\n";
+		sum = sum + i;
+	}
+	std::cout << "Suma" << sum << "\n";
 }
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
-	task1();
+	task3();
 }
