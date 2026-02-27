b5d7c95c1939c1dc93d1ff2dd8022293e625d60a
Sala213 <Sala213@ezse.pl>
2026-02-27 12:20:41 +0100

    Kilka projektów w pętli while (LoopWhileConsoleApplication 2)

---
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<windows.h>
+
+void task1()
+{
+
+
+
+
+
+
+}
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+
+	task1();
+}
--- Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
+++ Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
@@ -3,15 +3,155 @@
 
 void task1()
 {
+	int number;
+	std::cout << "Podaj liczbę\n";
+	std::cin >> number;
+
+	int sum = 0;
+
+	/*
+	if (number != 0)
+	{
+		do
+		{
+			int digit = number % 10;
+			sum = sum + digit;
+			number = number / 10;
+		} while (number != 0);
+	}
+	*/
+
+	while (number != 0)
+	{
+		int digit = number % 10;
+		sum = sum + digit;
+		number = number / 10;
+	}
+
+	std::cout << "Suma cyfr: " << sum << "\n";
+}
+//Napisz program, który policzy NWD dwóch liczb.
+void task2()
+{
+	int firstNumber, secondNumber;
+	std::cout << "Podaj pierwszą liczbę\n";
+	std::cin >> firstNumber;
+	std::cout << "Podaj drugą liczbę\n";
+	std::cin >> secondNumber;
+
+	int nwd = 1;
+
+	int min;
+	if (secondNumber < firstNumber)
+		min = secondNumber;
+	else
+		min = firstNumber;
+
+	//warunek ? wartość_zwracana_dla_prawdy : wartość_zwracana_dla_fałszu
+
+	min = (secondNumber < firstNumber) ? secondNumber : firstNumber;
+
+	int counter = 2;
+
+	while (counter <= min)
+	{
+		if (firstNumber % counter == 0
+			&& secondNumber % counter == 0)
+			nwd = counter;
+		counter++;
+	}
+
+	std::cout << "NWD = " << nwd << "\n";
+
+	//wersja 2
+	//NWD(a, b) = a				jeœli b = 0
+	//NWD(a, b) = NWD(b, a % b) jeœli b != 0
+	int a = firstNumber, b = secondNumber;
+	while (b != 0)
+	{
+		int tmp = a;
+		a = b;
+		b = tmp % b;
+	}
+	std::cout << "NWD = " << a << "\n";
+}
+//Napisz program, który wświetli "Hello world" 
+// tyle razy ile chce użytkownik
+void task3()
+{
+	int howManyTimes;
+	std::cout << "Podaj ile razy wyœwietliæ\n";
+	std::cin >> howManyTimes;
+
+	int i = 0;
+	while (i < howManyTimes)
+	{
+		std::cout << "Hello world\n";
+		i++;
+	}
+
+}
+//Napisz program, który wświetli 
+// liczby parzyste do podanej przez u¿ytkownika liczby
+void task4()
+{
+	int upperRange;
+	std::cout << "Podaj górną granicę do wyświetlenia\n";
+	std::cin >> upperRange;
+
+	int i = 0;
+	while (i <= upperRange)
+	{
+		std::cout << i << "\n";
+		i = i + 2;
+	}
+}
+//Napisz tabliczkê mno¿enia:
+/*
+1*1 = 1
+1*2 = 2
+1*3 = 3
+1*4 = 4
+.
+.
+1*9 = 9
+
+2*1 = 2
+2*2 = 4
+2*3 = 6
+.
+.
+2*9 = 18
+
+3*1 = 3
+3*2 = 6
+3*3 = 9
 
 
+9*9 = 81
 
+*/
+void task5()
+{
+	int firstNumber, secondNumber;
 
+	firstNumber = 0;
 
+	while (firstNumber <= 10)
+	{
+		secondNumber = 0;
+
+		while (secondNumber <= 10)
+		{
+			std::cout << firstNumber << " * " << secondNumber << " = " << firstNumber * secondNumber << "\n";
+			secondNumber++;
+		}
+		firstNumber++;
+	}
 }
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
-	task1();
+	task5();
 }   
